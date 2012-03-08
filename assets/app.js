d1f34aeae81bed981e07865dd5b066649722fdb2
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-09 01:09:41 +0600

    move to fancybox

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,21 @@
-$(function() {
-	$('.old-works a').lightBox({fixedNavigation:true});
+$(document).ready(function() {
+  // $(".old-works a").fancybox();
+  $(".old-works a").click(function(event) {
+    var piclist = [];
+    var curindex = 0;
+    $(".old-works a").each(function(idx) {
+      piclist.push(this.href);
+    });
+    $.fancybox(piclist, {
+        'padding'           : 0,
+        'margin'            : 0,
+        'centerOnScroll'    : true,
+        'transitionIn'      : 'none',
+        'transitionOut'     : 'none',
+        'type'              : 'image',
+        'changeFade'        : 0,
+        'index'             : 0
+    });
+    return false;
+  });
 });
